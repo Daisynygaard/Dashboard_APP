6df2d825a8e7a589d09c6944c854a9d82e5624a5
Qingqing Dai <Qingqing.Dai@nov.com>
2021-06-22 08:43:21 +0200

    modification_2

--- Dashboard_APP/Form1.Designer.cs
+++ Dashboard_APP/Form1.Designer.cs
@@ -4,14 +4,14 @@ namespace Dashboard_APP
     partial class Form1
     {
         /// <summary>
-        /// 必需的设计器变量。
+        ///Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清理所有正在使用的资源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -24,8 +24,8 @@ namespace Dashboard_APP
         #region Windows 窗体设计器生成的代码
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要修改
-        /// 使用代码编辑器修改此方法的内容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
